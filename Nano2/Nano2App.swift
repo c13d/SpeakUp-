24d925b2216c90bbc34404dfbcd0e9700b924a54
davin <christophorusdavin@gmail.com>
2022-07-22 14:34:18 +0700

    Initial Commit

---
+++ Nano2/Nano2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano2App.swift
+//  Nano2
+//
+//  Created by Christophorus Davin on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Nano2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
